a7ea160d6d1fd7453f37b20f3406b1d9af8ae922
Tim Park <timpark0807@gmail.com>
2019-06-30 15:37:48 -0700

    test to Post

--- tests/test_flaskapp.py
+++ tests/test_flaskapp.py
@@ -1,7 +1,7 @@
-from realtoranalysis import app, db
 import unittest
-from flask import url_for
-import os
+from flask_login import current_user
+from realtoranalysis import app, db
+from realtoranalysis.models import Post
 
 class PageLoadTestCase(unittest.TestCase):
     """"
@@ -44,9 +44,9 @@ class PageLoadTestCase(unittest.TestCase):
 
 class UserTestCase(unittest.TestCase):
 
-    ########################
+    ################################################################################################
     # Set up and Tear Down
-    ########################
+    ################################################################################################
 
     # Executed prior to each test
     def setUp(self):
@@ -56,29 +56,34 @@ class UserTestCase(unittest.TestCase):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
         self.app = app.test_client()
+        db.session.remove()
         db.drop_all()
         db.create_all()
 
     # executed after each test
     def tearDown(self):
+        # db.drop_all()
         pass
-
-    ########################
+    ################################################################################################
     # Helper Functions
-    ########################
+    ################################################################################################
 
     def register(self, username, email, password, confirm):
-        return self.app.post('/register', data=dict(username=username, email=email, password=password, confirm_password=confirm), follow_redirects=True)
+        return self.app.post('/register',
+                             data={'username': username,
+                                    'email': email,
+                                    'password': password,
+                                    'confirm_password': confirm})
 
     def login(self, email, password):
-        return self.app.post('/login', data=dict(email=email, password=password), follow_redirects=True)
+        return self.app.post('/login', data=dict(email=email, password=password))
 
     def logout(self):
         return self.app.get('/logout', follow_redirects=True)
 
-    ########################
+    ################################################################################################
     # Tests
-    ########################
+    ################################################################################################
 
     def test_user_registration(self):
         # Register page loads
@@ -86,20 +91,118 @@ class UserTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
         # send a post request to register with following inputs
-        response = self.app.post('/register', data={'username': 'user',
-                                                    'email': 'user@test.com',
-                                                    'password': 'password',
-                                                    'confirm_password': 'password'})
+        response = self.register('user', 'user@test.com', 'password', 'password')
+
         # Test that response is a redirect
         self.assertEqual(response.status_code, 302)
 
-        # send a post request to register for a user with duplicated username and email
-        response = self.app.post('/register', data={'username': 'user',
-                                                    'email': 'user@test.com',
-                                                    'password': 'pass',
-                                                    'confirm_password': 'pass'})
-        # Test that response is not a redirect
+    def test_duplicate_user_registration(self):
+
+        # send a post request to /register with following inputs
+        response = self.register('user', 'user@test.com', 'password', 'password')
+
+        # assert that this redirects us after successful registration
+        self.assertEqual(response.status_code, 302)
+
+        # send another post request to register with the username/email duplicated
+        response = self.register('user', 'user@test.com', 'password', 'password')
+
+        # assert this does not redirect us
         self.assertEqual(response.status_code, 200)
+
+    def test_user_login(self):
+
+        # register a user
+        self.register('user', 'user@test.com', 'password', 'password')
+
+        # Login page loads
+        response = self.app.get('/login')
+        self.assertEqual(response.status_code, 200)
+
+        # send a post request to login with registered user info
+        response = self.login('user@test.com', 'password')
+
+        # Test that response is a redirect
+        self.assertEqual(response.status_code, 302)
+
+    def test_analyze(self):
+        with self.app:
+            self.register('user', 'user@test.com', 'password', 'password')
+            self.login('user@test.com', 'password')
+            user = current_user
+            post = Post(title='Test',
+                        url='test.com',
+                        street='21 West Arrellaga',
+                        city='Santa Barbara',
+                        state='CA',
+                        zipcode='93101',
+
+                        type='Single Family',
+                        year='1994',
+                        bed='4',
+                        bath='3',
+                        sqft='2300',
+
+                        price='100000',
+                        term='30',
+                        down='20',
+                        interest='5',
+                        closing='3',
+
+                        rent='1500',
+                        other='0',
+                        expenses='50',
+                        vacancy='10',
+                        appreciation='3',
+
+                        mortgage='429',
+                        outofpocket='22000',
+                        cap_rate='8',
+                        coc='7',
+                        operating_income='1350',
+                        operating_expense='750',
+                        cash_flow='131',
+
+                        author=user)
+
+            db.session.add(post)
+            db.session.commit()
+            self.assertEqual(post.title, 'Test')
+
+            # response = self.app.post('/analyze',  data=dict(title='Test',
+            #                                                 url='test.com',
+            #                                                 street='21 West Arrellaga',
+            #                                                 city='Santa Barbara',
+            #                                                 state='CA',
+            #                                                 zipcode='93101',
+            #
+            #                                                 type='Single Family',
+            #                                                 year='1994',
+            #                                                 bed='4',
+            #                                                 bath='3',
+            #                                                 sqft='2300',
+            #
+            #                                                 price='100000',
+            #                                                 term='30',
+            #                                                 down='20',
+            #                                                 interest='5',
+            #                                                 closing='3',
+            #
+            #                                                 rent='1500',
+            #                                                 other='0',
+            #                                                 expenses='50',
+            #                                                 vacancy='10',
+            #                                                 appreciation='3',
+            #
+            #                                                 mortgage='429',
+            #                                                 outofpocket='22000',
+            #                                                 cap_rate='8',
+            #                                                 coc='7',
+            #                                                 operating_income='1350',
+            #                                                 operating_expense='750',
+            #                                                 cash_flow='131',
+            #
+            #                                                 author=user))
 
 if __name__ == '__main__':
     unittest.main()
